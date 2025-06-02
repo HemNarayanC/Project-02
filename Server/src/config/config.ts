df57f1678aa69a54b1d439c140c2128d03d25393
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-02 14:27:40 +0545

    config

--- Server/src/config/config.ts
+++ Server/src/config/config.ts
@@ -1,6 +1,12 @@
-import {config} from 'dotenv'
-config()
+import { config } from 'dotenv';
+config();
 
 export const envConfig = {
-    portNumber: process.env.PORT
-}
+  portNumber: process.env.PORT,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbDialect: process.env.DB_DIALECT
+};
